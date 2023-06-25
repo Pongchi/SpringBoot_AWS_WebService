ba46029c91f75914fb480788d4d063e706b7039a
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-06-25 20:24:06 +0900

    Real First Commit!!

---
+++ src/test/java/com/pongchi/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pongchi.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
