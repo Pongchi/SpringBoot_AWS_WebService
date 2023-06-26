a4f27a67a64c3894e1d8e51ccabf37cfa537ff6b
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-06-26 11:22:31 +0000

    Page 99

---
+++ src/main/java/com/pongchi/blog/web/HelloController.java
@@ -0,0 +1,26 @@
+package com.pongchi.blog.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pongchi.blog.web.dto.HelloResponseDto;
+
+/**
+ * HelloController
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        
+        return new HelloResponseDto(name, amount);
+    }
+}
