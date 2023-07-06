ec2e40b5bdd89190f3a3ad2fcc38792902a5aa95
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-07-06 11:00:26 +0000

    add deleteMapping annotation

--- src/main/java/com/pongchi/blog/web/PostsApiController.java
+++ src/main/java/com/pongchi/blog/web/PostsApiController.java
@@ -1,5 +1,6 @@
 package com.pongchi.blog.web;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
