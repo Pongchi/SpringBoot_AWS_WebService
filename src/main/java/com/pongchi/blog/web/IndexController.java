f977e4c43307d6e0f32c6f42df1bc5b632db2be7
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-07-10 10:31:43 +0000

    add OAuth2 with Google and User Domain

--- src/main/java/com/pongchi/blog/web/IndexController.java
+++ src/main/java/com/pongchi/blog/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.pongchi.blog.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.pongchi.blog.config.auth.dto.SessionUser;
 import com.pongchi.blog.service.posts.PostsService;
 import com.pongchi.blog.web.dto.PostsResponseDto;
 
@@ -15,10 +18,17 @@ import lombok.RequiredArgsConstructor;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
     
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
