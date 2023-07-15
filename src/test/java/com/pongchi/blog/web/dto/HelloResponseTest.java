e008336cb105732c5abd50a5dc47fc445f53e0e8
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-07-15 08:12:49 +0000

    bug fix

--- src/test/java/com/pongchi/blog/web/dto/HelloResponseTest.java
+++ src/test/java/com/pongchi/blog/web/dto/HelloResponseTest.java
@@ -1,8 +1,8 @@
 package com.pongchi.blog.web.dto;
 
-
-import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
 
 public class HelloResponseTest {
     
