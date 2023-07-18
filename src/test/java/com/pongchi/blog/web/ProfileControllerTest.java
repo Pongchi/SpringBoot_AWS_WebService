57323c7476590efcfe3fa2480df810d8405c4def
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-07-18 14:35:58 +0000

    why me!

--- src/test/java/com/pongchi/blog/web/ProfileControllerTest.java
+++ src/test/java/com/pongchi/blog/web/ProfileControllerTest.java
@@ -24,7 +24,7 @@ public class ProfileControllerTest {
 
     @Test
     public void profile_is_not_need_authorization() throws Exception {
-        String expected = "default";
+        String expected = "real";
 
         ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
